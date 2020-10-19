c5cd1466375ff6f6bd4c3ba91a111fafaf039580
Darlan Oliveira Santos <55238348+darlantn45@users.noreply.github.com>
2020-10-19 11:47:01 -0300

    Add files via upload

---
+++ Exercicio/src/Exercicio1.java
@@ -0,0 +1,8 @@
+//Programa de impressão
+public class Exercicio1 //sempre começa com o nome doo arquivo
+{
+	//método usado para escrever
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	}//fim do método
+}//fim da classe Exercicio1
